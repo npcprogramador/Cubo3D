9b9ed3763e8006c64a24bf3ae29cfe54d4e30d61
npcwalison <gwalisonla@gmail.com>
2023-04-18 09:30:59 -0300

    update

---
+++ script.js
@@ -0,0 +1,30 @@
+const eixo = document.querySelector('.eixo');
+
+let horizontal = 47;
+let vertical = 55;
+var pixel = 6;
+
+eixo.style = `transform: rotateX(${vertical}deg) rotateY(${horizontal}deg) rotateZ(0deg);`
+
+function control(val) {
+    switch (val) {
+        case 'up':
+            //alert(val)
+            vertical += pixel
+            break;
+        case 'left':
+            //alert(val)
+            horizontal -= pixel
+            break;
+        case 'down':
+            //alert(val)
+            vertical += pixel
+            break;
+        case 'right':
+            //alert(val)
+            horizontal -= pixel
+            break;
+    }
+}
+
+vertical = pixel
